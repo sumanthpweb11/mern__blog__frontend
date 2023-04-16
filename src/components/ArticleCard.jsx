dd15fd58737d0639ec8b50996bca5cefad952387
sumanthpweb11 <sumanthpweb11@gmail.com>
2023-04-16 19:21:06 +0530

    Article card and Footer added

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,11 +1,54 @@
+import { BsCheckLg } from "react-icons/bs";
 import { Images } from "../constants";
 
-const ArticleCard = () => {
+const ArticleCard = ({ className }) => {
   return (
     <div
-      className={`  rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px]`}
+      className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className} `}
     >
-      <img src={Images.Post1} alt="title" />
+      <img
+        className="w-full object-cover object-center h-auto md:h-52 lg:h-48 xl:h-60"
+        src={Images.Post1}
+        alt="title"
+      />
+
+      <div className="p-5">
+        <h2 className="font-roboto font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px]">
+          Future of Work
+        </h2>
+        <p className="text-dark-light mt-3 text-sm md:text-lg ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
+          suscipit.
+        </p>
+        {/*PROFILE PART AND DATE OF PUBLISHING */}
+        <div className="flex justify-between flex-nowrap items-center mt-6">
+          <div className="flex items-center gap-x-2 md:gap-x-2.5">
+            <img
+              className="w-9 h-9 md:w-10 md:h-10"
+              src={Images.PostProfile}
+              alt="post profile"
+            />
+            <div className="flex flex-col">
+              <h4 className="font-bold italic text-dark-soft text-sm md:text-base">
+                John Smilga
+              </h4>
+              <div className="flex items-center gap-x-2">
+                <span className="bg-[#36B37E] w-fit bg-opacity-20 p-1.5 rounded-full">
+                  <BsCheckLg className="w-1.5 h-1.5 text-[#36B37E]" />
+                </span>
+                <span className="italic text-dark-light text-xs md:text-sm">
+                  Verified Writer
+                </span>
+              </div>
+            </div>
+          </div>
+          <span className="font-bold text-dark-light italic text-sm md:text-base">
+            02 May
+          </span>
+        </div>
+
+        {/*PROFILE PART AND DATE OF PUBLISHING */}
+      </div>
     </div>
   );
 };
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,11 +1,13 @@
 import Logo from "../assets/Logo.png";
 import Hero from "../assets/hero.svg";
 import Post1 from "../assets/posts/Post1.png";
+import PostProfile from "../assets/posts/post-profile.jpg";
 
 const Images = {
   Logo,
   Hero,
   Post1,
+  PostProfile,
 };
 
 export default Images;
